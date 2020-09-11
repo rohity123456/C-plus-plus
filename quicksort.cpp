cbab9c05cb20b749b082a9ef9e23975ba84bcb0c
Rohit Yadav <rohity123456@gmail.com>
2020-09-12 01:11:12 +0530

    quicksort algorithm Implementation

---
+++ quicksort.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+void quicksort(vector<int> &unsortedArray, int start, int end)
+{
+    if (start >= end)
+        return;
+    int pivot = start;
+    int left = pivot + 1;
+    int right = end;
+    while (left <= right)
+    {
+        if (unsortedArray[left] > unsortedArray[pivot] && unsortedArray[right] < unsortedArray[pivot])
+        {
+            swap(unsortedArray[left], unsortedArray[right]);
+            left++;
+            right--;
+        }
+        else if (unsortedArray[left] <= unsortedArray[pivot])
+            left++;
+        else if (unsortedArray[right] >= unsortedArray[pivot])
+            right--;
+    }
+    swap(unsortedArray[pivot], unsortedArray[right]);
+    quicksort(unsortedArray, start, right - 1);
+    quicksort(unsortedArray, right + 1, end);
+}
+
+int main()
+{
+    int n, inp;
+    cin >> n;
+    vector<int> unsortedArray;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> inp;
+        unsortedArray.push_back(inp);
+    }
+    quicksort(unsortedArray, 0, n - 1);
+    for (auto i : unsortedArray)
+    {
+        cout << i << " ";
+    }
+}
